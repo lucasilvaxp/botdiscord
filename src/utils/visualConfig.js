033eb45cb71f1e9b152cfa4b9d5c6778aeb1e179
Lucas Silva <lucass.uchiha2002@gmail.com>
2026-05-27 09:30:11 -0300

    Add visual configuration for colors and assets

---
+++ src/utils/visualConfig.js
@@ -0,0 +1,16 @@
+module.exports = {
+    colors: {
+        sorteada: '#8a2be2', // Roxo
+        desafio: '#ffd700', // Dourado
+        started: '#00ff00', // Verde
+        finished: '#ff0000', // Vermelho
+        default: '#2b2d31'
+    },
+    assets: {
+        logo: 'https://i.imgur.com/AfFp7pu.png', // Substituir pelo link real se disponível
+        thumbnail: 'https://i.imgur.com/AfFp7pu.png', // GIF ou Logo
+        footerIcon: 'https://i.imgur.com/AfFp7pu.png'
+    },
+    botName: 'Arena Matchmaking',
+    systemName: 'Sistema de Filas Ranked'
+};
